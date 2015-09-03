2813baf33623f336c6128bc5edbe5f01998f4a91
Michael Liang <michaelpliang@gmail.com>
2015-09-02 23:51:18 -0700

    final updates

--- main.js
+++ main.js
@@ -11,12 +11,9 @@ $(document).ready(init);
 	}
 
 	function goClicked(event) {
-		console.log('go clicked');
 		$('#displayArea').remove();
 		var city = $('#location').val();
-
 		console.log(city);
-
 		if (city === '') {
 			city = "autoip";
 		}
@@ -24,12 +21,14 @@ $(document).ready(init);
 		$.getJSON('http://api.wunderground.com/api/ed7493f8cb5454e1/geolookup/q/' + city + '.json')
 			.success(function(data){
 				console.log(data);
+				
 				if (data.response.error) {
 					console.log("error: ", data);
 					$('.displayCity').text(data.response.error.description);
 				} else {
 				$('.displayCity').text('');
-				$('.displayCity').append(data.location.city + ", " + data.location.state);
+				var country = (data.location.country === 'US') ? data.location.state : data.location.country;
+				$('.displayCity').append(data.location.city + ", " + country);
 				}
 			})
 			.fail(function(error){
@@ -39,11 +38,15 @@ $(document).ready(init);
 
 		$.getJSON('http://api.wunderground.com/api/f072e1fa016ca1ac/forecast10day/q/' + city + '.json')
 			.success(function(data){
+
+				if (data.response.error){
+					console.log("city not found");
+				} else {
 				$('.container').append('<div class="animated rollIn" id="displayArea"></div>');
-			console.log(data);
+				console.log(data);
 				data.forecast.txt_forecast.forecastday.forEach(function(val, index) {
 				
-					if (index%2 === 0) {
+					if (index%2 === 0 && index<14) {
 					
 						$('#displayArea').append($("<h4>" + val.title + "</h4>"), 
 							$('<img src="' + val.icon_url + '"></img>'), "  ",
@@ -51,9 +54,11 @@ $(document).ready(init);
 					}
 				});
 				$('#location').val("");
+				}
 			})
 			.fail(function(error){
 				console.log(error);
 			});
-	}
+		}
+	
 
