30706980663d78707afc32458e5369d6ff86fe68
Michael Liang <michaelpliang@gmail.com>
2015-10-08 21:09:56 -0700

    ux update

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 
+var $ = require('jquery');
+// var bootstrap = require('bootstrap');
+require("./style.css");
+require("./animate.css");
+
 $(document).ready(init);
 
 	function init() {
-		$('#go').click(goClicked);
-		$('#location').keyup(function(e) {
+		// $('#go').click(goClicked);
+		$('#location').on('keyup', function(e) {
 			if (e.keyCode === 13) {
 	      		goClicked();
 	  		}
@@ -12,6 +17,7 @@ $(document).ready(init);
 
 	function goClicked(event) {
 		$('#displayArea').remove();
+		$('#instructions').addClass('hidden');
 		var city = $('#location').val();
 		console.log(city);
 		if (city === '') {
@@ -21,38 +27,38 @@ $(document).ready(init);
 		$.getJSON('http://api.wunderground.com/api/f072e1fa016ca1ac/forecast10day/q/' + city + '.json')
 			.success(function(data){
 
-						$.getJSON('http://api.wunderground.com/api/ed7493f8cb5454e1/geolookup/q/' + city + '.json')
-							.success(function(data){
-							console.log(data);
-				
-								if (data.response.error) {
-									console.log("error: ", data);
-									$('.displayCity').text(data.response.error.description);
-									$('#location').val("");
-								} else {
-								$('.displayCity').text('');
-								var country = (data.location.country === 'US') ? data.location.state : data.location.country;
-								$('.displayCity').append(data.location.city + ", " + country);
-								}
-							})
-							.fail(function(error){
-								console.log("fatal error", error);
-								$('.displayCity').text("Please enter a valid city");
-								$('#location').val("");
-							});
+				$.getJSON('http://api.wunderground.com/api/ed7493f8cb5454e1/geolookup/q/' + city + '.json')
+					.success(function(data){
+					console.log(data);
+
+						if (data.response.error) {
+							console.log("error: ", data);
+							$('.displayCity').text(data.response.error.description);
+							$('#location').val("");
+						} else {
+						$('.displayCity').text('');
+						var country = (data.location.country === 'US') ? data.location.state : data.location.country;
+						$('.displayCity').append(data.location.city + ", " + country);
+						}
+					})
+					.fail(function(error){
+						console.log("fatal error", error);
+						$('.displayCity').text("Please enter a valid city");
+						$('#location').val("");
+					});
 
 				if (data.response.error){
 					console.log("city not found");
 				} else {
-				$('.container').append('<div class="animated rollIn" id="displayArea"></div>');
+				$('.weatherapp').append('<div class="animated pulse" id="displayArea"></div>');
 				console.log(data);
 				data.forecast.txt_forecast.forecastday.forEach(function(val, index) {
-				
+
 					if (index%2 === 0 && index<14) {
-					
-						$('#displayArea').append($("<h4>" + val.title + "</h4>"), 
-							$('<img src="' + val.icon_url + '"></img>'), "  ",
-							val.fcttext, '</br>');
+
+						$('#displayArea').append($("<h4>" + val.title + "</h4>"),
+							$('<img src="' + val.icon_url + '" class="thumb"></img>'), "  ",
+							val.fcttext, '<hr/>');
 					}
 				});
 				$('#location').val("");
@@ -62,5 +68,3 @@ $(document).ready(init);
 				console.log(error);
 			});
 		}
-	
-
